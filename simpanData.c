cc4010faacc546564c42f0ed6c95fbaca3b5ca77
Bilqhis Rhaina Sasmita <bilqhisrhaina87@gmail.com>
2025-12-18 23:18:00 +0700

    Update simpanData.c
    mencoba

--- simpanData.c
+++ simpanData.c
@@ -1,3 +1,22 @@
+void tugasBilqhis2() {
+    if (jumlahBuah == 0) {
+        printf("Belum ada data buah.\n");
+        return;
+    }
+
+    for (int i = 0; i < jumlahBuah; i++) {
+        printf("%d. [%s] %s | Stok: %d | Harga: %.2f\n",
+               i+1,
+               daftarBuah[i].kode,
+               daftarBuah[i].nama,
+               daftarBuah[i].stok,
+               daftarBuah[i].harga);
+    }
+}
+
+
+//agar data terimpan
+
 int main() {
     int pilihan;
     loadData();
