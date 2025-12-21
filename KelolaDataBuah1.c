9b55aeade43eced1632c10989d4c5d1a4e136054
Bilqhis Rhaina Sasmita <bilqhisrhaina87@gmail.com>
2025-12-21 11:13:36 +0700

    Update KelolaDataBuah1.c
    ubah tambah spasi

--- KelolaDataBuah1.c
+++ KelolaDataBuah1.c
@@ -2,7 +2,7 @@ void tugasBilqhis1() {
     char kodeInput[20];
     int found = -1;
 
-    printf("\n=== Tambah Buah Baru / Update Stok ===\n");
+    printf("\n=== Tambah Buah Baru ===\n");
     printf("Kode  : ");
     scanf("%s", kodeInput);
 
@@ -15,51 +15,48 @@ void tugasBilqhis1() {
     }
 
     if (found != -1) {
-        // Buah sudah ada → update stok
-        int tambahStok;
-        do {
-            printf("Buah sudah ada!\nTambah stok: ");
-            scanf("%d", &tambahStok);
-            if (tambahStok < 0) {
-                printf("Stok tidak boleh negatif!\n");
-            }
-        } while (tambahStok < 0);
-
-        daftarBuah[found].stok += tambahStok;
-        printf("Stok berhasil diperbarui! Stok sekarang: %d\n", daftarBuah[found].stok);
-
-    } else {
-        // Buah baru → input data
-        strcpy(daftarBuah[jumlahBuah].kode, kodeInput);
-
-        printf("Nama  : ");
-        scanf("%s", daftarBuah[jumlahBuah].nama);
-
-        int stokInput;
-        do {
-            printf("Stok  : ");
-            scanf("%d", &stokInput);
-            if (stokInput < 0) {
-                printf("Stok tidak boleh negatif!\n");
-            }
-        } while (stokInput < 0);
-        daftarBuah[jumlahBuah].stok = stokInput;
-
-        float hargaInput;
-        do {
-            printf("Harga : ");
-            scanf("%f", &hargaInput);
-            if (hargaInput < 0) {
-                printf("Harga tidak boleh negatif!\n");
-            }
-        } while (hargaInput < 0);
-        daftarBuah[jumlahBuah].harga = hargaInput;
-
-        jumlahBuah++;
-        printf("Buah baru berhasil ditambahkan!\n");
+        printf("Buah dengan kode '%s' sudah ada. Tidak ada perubahan stok.\n", kodeInput);
+        return;
     }
-}
 
+    // Buah baru → input data
+    strcpy(daftarBuah[jumlahBuah].kode, kodeInput);
+
+    getchar(); // bersihkan sisa newline dari scanf
+    printf("Nama  : ");
+    fgets(daftarBuah[jumlahBuah].nama,
+           sizeof(daftarBuah[jumlahBuah].nama),
+           stdin);
+
+    // hapus karakter newline
+    daftarBuah[jumlahBuah].nama[
+        strcspn(daftarBuah[jumlahBuah].nama, "\n")
+    ] = '\0';
+
+    int stokInput;
+    do {
+        printf("Stok  : ");
+        scanf("%d", &stokInput);
+        if (stokInput < 0) {
+            printf("Stok tidak boleh negatif!\n");
+        }
+    } while (stokInput < 0);
+    daftarBuah[jumlahBuah].stok = stokInput;
+
+    float hargaInput;
+    do {
+        printf("Harga : ");
+        scanf("%f", &hargaInput);
+        if (hargaInput < 0) {
+            printf("Harga tidak boleh negatif!\n");
+        }
+    } while (hargaInput < 0);
+    daftarBuah[jumlahBuah].harga = hargaInput;
+
+    jumlahBuah++;
+    printf("Buah baru berhasil ditambahkan!\n");
+    saveData();
+}
 
 void tugasBilqhis2() {
     printf("\n=== Daftar Buah ===\n");
@@ -78,5 +75,6 @@ void tugasBilqhis2() {
     }
 
 }
+
 
 
